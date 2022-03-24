11ea4828a9b4bfd6ef6aa67a724b4fc88ae459ab
Thomas Vargas <tavargas2012irc@gmail.com>
2022-03-24 09:38:28 -0500

    blog list init.

---
+++ blog-backend/utils/list_helper.js
@@ -0,0 +1,24 @@
+const dummy = (blogs) => {
+  if (blogs) {
+    return 1
+  } else {
+    return 2
+  }
+}
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+}
+
+const favoriteBlog = (blogs) => {
+  return blogs.reduce((previous, current) => {
+    return previous.likes > current.likes ? previous : current
+  }, 0)
+}
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog
+}
